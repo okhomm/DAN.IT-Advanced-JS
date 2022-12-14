9929f0d840bb2fd52a3b4538917c7f0e75ccc9d7
Khomyshyn Oleh <khomyshyn.oleg@gmail.com>
2022-12-14 09:00:10 +0200

    Some err fix

--- HW1/js/Programmer.js
+++ HW1/js/Programmer.js
@@ -1,8 +1,17 @@
 class Programmer extends Employee {
   constructor(name, age, salary, lang) {
     super(name, age, salary);
-    this.lang = lang;
+    this._lang = lang;
   }
+
+  set lang(value) {
+    return this._lang += ', ' + value;
+  }
+
+  get lang() {
+    return this._lang;
+  }
+
   get salary() {
     return this._salary * 3;
   }
--- HW1/js/script.js
+++ HW1/js/script.js
@@ -1,6 +1,6 @@
-const user1 = new Programmer('Oleh', 36, 1000, ['ua', 'eng']);
-const user2 = new Programmer('Ivan', 25, 800, ['ua', 'eng', 'de']);
-const user3 = new Programmer('Alex', 18, 500, ['ua', 'eng', 'fr']);
+const user1 = new Programmer('Oleh', 36, 1000, 'JavaScript');
+const user2 = new Programmer('Ivan', 25, 800, 'JavaScript, Java, PHP');
+const user3 = new Programmer('Alex', 18, 500, 'JavaScript, Java, Pyton');
 
 console.log(user1);
 console.log(user2);
